f2e0c942e88b5953a5970960c48a02d6fbf95d07
Venkata Naga Kartik Pidatala <vpidatala@microsoft.com>
2025-04-02 15:37:25 -0700

    more changes

--- test/e2e/retina_e2e_test.go
+++ test/e2e/retina_e2e_test.go
@@ -3,6 +3,8 @@
 package retina
 
 import (
+	"crypto/rand"
+	"math/big"
 	"os"
 	"path/filepath"
 	"testing"
@@ -11,7 +13,6 @@ import (
 	"github.com/microsoft/retina/test/e2e/common"
 	"github.com/microsoft/retina/test/e2e/framework/helpers"
 	"github.com/microsoft/retina/test/e2e/framework/types"
-	"github.com/microsoft/retina/test/e2e/infra"
 	jobs "github.com/microsoft/retina/test/e2e/jobs"
 	"github.com/stretchr/testify/require"
 )
@@ -21,65 +22,67 @@ func TestE2ERetina(t *testing.T) {
 	ctx, cancel := helpers.Context(t)
 	defer cancel()
 
+	// Truncate the username to 8 characters
+	clusterName := common.ClusterNameForE2ETest(t)
+
+	subID := os.Getenv("AZURE_SUBSCRIPTION_ID")
+	require.NotEmpty(t, subID)
+
+	location := os.Getenv("AZURE_LOCATION")
+	if location == "" {
+		nBig, err := rand.Int(rand.Reader, big.NewInt(int64(len(common.AzureLocations))))
+		if err != nil {
+			t.Fatalf("Failed to generate a secure random index: %v", err)
+		}
+		location = common.AzureLocations[nBig.Int64()]
+	}
+
+	rg := os.Getenv("AZURE_RESOURCE_GROUP")
+	if rg == "" {
+		// Use the cluster name as the resource group name by default.
+		rg = clusterName
+	}
+
 	cwd, err := os.Getwd()
 	require.NoError(t, err)
 
 	// Get to root of the repo by going up two directories
 	rootDir := filepath.Dir(filepath.Dir(cwd))
 
-	//hubblechartPath := filepath.Join(rootDir, "deploy", "hubble", "manifests", "controller", "helm", "retina")
+	chartPath := filepath.Join(rootDir, "deploy", "legacy", "manifests", "controller", "helm", "retina")
+	hubblechartPath := filepath.Join(rootDir, "deploy", "hubble", "manifests", "controller", "helm", "retina")
+	profilePath := filepath.Join(rootDir, "test", "profiles", "advanced", "values.yaml")
+	kubeConfigFilePath := filepath.Join(rootDir, "test", "e2e", "test.pem")
 
-	err = jobs.LoadGenericFlags().Run()
-	require.NoError(t, err, "failed to load generic flags")
-
-	if *common.KubeConfig == "" {
-		*common.KubeConfig = infra.CreateAzureTempK8sInfra(ctx, t, rootDir)
-	}
+	// CreateTestInfra
+	createTestInfra := types.NewRunner(t, jobs.CreateTestInfra(subID, rg, clusterName, location, kubeConfigFilePath, *common.CreateInfra))
+	createTestInfra.Run(ctx)
 
 	// Install Ebpf and XDP
-	installEbpfAndXDP := types.NewRunner(t, jobs.InstallEbpfXdp(common.KubeConfigFilePath(rootDir)))
+	installEbpfAndXDP := types.NewRunner(t, jobs.InstallEbpfXdp(kubeConfigFilePath))
 	installEbpfAndXDP.Run(ctx)
 
 	time.Sleep(10 * time.Minute)
 
-	// Load and pin BPF Maps
-	loadAndPinWinBPFJob := types.NewRunner(t, jobs.LoadAndPinWinBPFJob(common.KubeConfigFilePath(rootDir)))
-	loadAndPinWinBPFJob.Run(ctx)
+	// Load BPF Maps
+	loadWinBPFMapsJob := types.NewRunner(t, jobs.LoadWinBPFMapsJob(kubeConfigFilePath))
+	loadWinBPFMapsJob.Run(ctx)
+
+	t.Cleanup(func() {
+		if *common.DeleteInfra {
+			_ = jobs.DeleteTestInfra(subID, rg, clusterName, location).Run()
+		}
+	})
 
 	// Install and test Retina basic metrics
-
-	basicMetricsE2E := types.NewRunner(t,
-		jobs.InstallAndTestRetinaBasicMetrics(
-			common.KubeConfigFilePath(rootDir),
-			common.RetinaChartPath(rootDir),
-			common.TestPodNamespace),
-	)
+	basicMetricsE2E := types.NewRunner(t, jobs.InstallAndTestRetinaBasicMetrics(kubeConfigFilePath, chartPath, common.TestPodNamespace))
 	basicMetricsE2E.Run(ctx)
 
-	// Upgrade and test Retina with advanced metrics
-	advanceMetricsE2E := types.NewRunner(t,
-		jobs.UpgradeAndTestRetinaAdvancedMetrics(
-			common.KubeConfigFilePath(rootDir),
-			common.RetinaChartPath(rootDir),
-			common.RetinaAdvancedProfilePath(rootDir),
-			common.TestPodNamespace),
-	)
+	//Upgrade and test Retina with advanced metrics
+	advanceMetricsE2E := types.NewRunner(t, jobs.UpgradeAndTestRetinaAdvancedMetrics(kubeConfigFilePath, chartPath, profilePath, common.TestPodNamespace))
 	advanceMetricsE2E.Run(ctx)
 
-	/*
-		// unpin BPF Maps
-		unloadAndPinWinBPFJob := types.NewRunner(t, jobs.UnLoadAndPinWinBPFJob(common.KubeConfigFilePath(rootDir)))
-		unloadAndPinWinBPFJob.Run(ctx)
-
-		// Install and test Hubble basic metrics
-		validatehubble := types.NewRunner(t,
-			jobs.ValidateHubble(
-				common.KubeConfigFilePath(rootDir),
-				hubblechartPath,
-				common.TestPodNamespace),
-		)
-		validatehubble.Run(ctx)
-
-	*/
-
+	// Install and test Hubble basic metrics
+	validatehubble := types.NewRunner(t, jobs.ValidateHubble(kubeConfigFilePath, hubblechartPath, common.TestPodNamespace))
+	validatehubble.Run(ctx)
 }
