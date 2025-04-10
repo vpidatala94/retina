526edf08a039547a65d246c737ab9003203b6ab2
Venkata Naga Kartik Pidatala <vpidatala@microsoft.com>
2025-04-09 22:10:45 -0700

    comment

--- test/e2e/jobs/jobs.go
+++ test/e2e/jobs/jobs.go
@@ -17,7 +17,7 @@ import (
 
 func CreateTestInfra(subID, rg, clusterName, location, kubeConfigFilePath string, createInfra bool) *types.Job {
 	job := types.NewJob("Create e2e test infrastructure")
-	createInfra = false
+	createInfra = true
 	if createInfra {
 		job.AddStep(&azure.CreateResourceGroup{
 			SubscriptionID:    subID,
