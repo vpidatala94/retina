b177f59f3da7eed4369cc01d7c7495af1f2b2eed
Venkata Naga Kartik Pidatala <vpidatala@microsoft.com>
2025-04-10 02:27:11 -0700

    reenable retina basic + advanced + hubble

--- test/e2e/retina_e2e_test.go
+++ test/e2e/retina_e2e_test.go
@@ -49,8 +49,8 @@ func TestE2ERetina(t *testing.T) {
 	rootDir := filepath.Dir(filepath.Dir(cwd))
 
 	chartPath := filepath.Join(rootDir, "deploy", "standard", "manifests", "controller", "helm", "retina")
-	//hubblechartPath := filepath.Join(rootDir, "deploy", "hubble", "manifests", "controller", "helm", "retina")
-	//profilePath := filepath.Join(rootDir, "test", "profiles", "advanced", "values.yaml")
+	hubblechartPath := filepath.Join(rootDir, "deploy", "hubble", "manifests", "controller", "helm", "retina")
+	profilePath := filepath.Join(rootDir, "test", "profiles", "advanced", "values.yaml")
 	kubeConfigFilePath := filepath.Join(rootDir, "test", "e2e", "test.pem")
 
 	err = jobs.LoadGenericFlags().Run()
@@ -88,6 +88,6 @@ func TestE2ERetina(t *testing.T) {
 	installAndTestWinBPFMetricsE2E.Run(ctx)
 
 	// Install and test Hubble basic metrics
-	//validatehubble := types.NewRunner(t, jobs.ValidateHubble(kubeConfigFilePath, hubblechartPath, common.TestPodNamespace))
-	//validatehubble.Run(ctx)
+	validatehubble := types.NewRunner(t, jobs.ValidateHubble(kubeConfigFilePath, hubblechartPath, common.TestPodNamespace))
+	validatehubble.Run(ctx)
 }
